f1928132dfba677d518424a2b7e19238815ec77a
Daniel Jere <107412261+DanielJere2916@users.noreply.github.com>
2025-02-04 01:58:46 +0000

    add user registration endpoint and validation request

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\API\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', TaskController::class);
+    
 });
+Route::post('register', RegisterController::class)->name('register');
